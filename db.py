501824ca418852955ca719f25d699ffb0236a27c
Patrick Mylund Nielsen <patrick@patrickmylund.com>
2011-02-04 22:48:47 +0100

    fixed sitemap?

--- db.py
+++ db.py
@@ -106,10 +106,13 @@ def getSitemap():
     if val is None:
         host = os.environ['HTTP_HOST'] if os.environ.get('HTTP_HOST') else os.environ['SERVER_NAME']
         sitemap = []
+        sitemap.append('<?xml version="1.0" encoding="UTF-8"?>')
+        sitemap.append('<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">')
         quotes = getQuotes()
         for quote in quotes:
-            entry = '<url>%s/%s/%s</url>' % (host, quote.author.slug, quote.key().id())
+            entry = '<url><loc>%s/%s/%s</loc></url>' % (host, quote.author.slug, quote.key().id())
             sitemap.append(entry)
+        sitemap.append('</urlset>')
         val = u'\r\n'.join(sitemap)
         memcache.set(mc_key, val, settings.page_cache_duration)
     return val
