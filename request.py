4705f8fe1428cd523728a4595ce4890e3cf38c09
Patrick Mylund Nielsen <patrick@patrickmylund.com>
2011-02-05 19:25:19 +0100

    Allow HEAD, facebook insights tag

--- request.py
+++ request.py
@@ -2,5 +2,9 @@ from google.appengine.ext import webapp
 
 class Request(webapp.RequestHandler):
 
+    def head(self, *args, **kwargs):
+        self.get(*args, **kwargs)
+        self.response.clear()
+
     def send(self, data):
         return self.response.out.write(data)
