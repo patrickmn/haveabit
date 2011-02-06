7b338b3278d0b69adfa05e11a810c32ff67a4d55
Patrick Mylund Nielsen <patrick@patrickmylund.com>
2011-02-06 23:55:43 +0100

    .

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ import os
 
 use_strftime = False
 strftime_format = '%b %e, %Y'
-long_cache_control = 'public, max-age=86400' # Default browser Cache-Control for pages that rarely change
 cache_control = 'public, max-age=3600'       # Default browser Cache-Control for pages that might change
+long_cache_control = 'public, max-age=86400' # Default browser Cache-Control for pages that rarely change
 page_cache_duration = 2591999                # How many seconds to cache (static) rendered pages (max: 2591999)
 quotelist_cache_duration = 2591999           # How many seconds to cache lists of quotes (max: 2591999)
 # quotelist_cache_duration = 3600
