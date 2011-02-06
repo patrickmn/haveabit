f1eb8503827b7800c1f1b25a4658aeda647d8f07
Patrick Mylund Nielsen <patrick@patrickmylund.com>
2011-02-06 22:55:57 +0100

    cache duration can be 2591999 max, not 2592000 (1 month)

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ import os
 
 use_strftime = False
 strftime_format = '%b %e, %Y'
-page_cache_duration = 2592000      # How many seconds to cache (static) rendered pages
-quotelist_cache_duration = 2592000 # How many seconds to cache lists of quotes
+page_cache_duration = 2591999      # How many seconds to cache (static) rendered pages (max: 2591999)
+quotelist_cache_duration = 2591999 # How many seconds to cache lists of quotes (max: 2591999)
 # quotelist_cache_duration = 3600
 
 address = 'http://' + os.environ['HTTP_HOST'] if os.environ.get('HTTP_HOST') else os.environ['SERVER_NAME']
